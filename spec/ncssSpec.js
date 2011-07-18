48de0c90970d4079d6138dbd5d380c8263b690cd
Wil Asche <wasche@tripadvisor.com>
2011-07-18 10:25:58 -0400

    add remaining tests cases

--- spec/ncssSpec.js
+++ spec/ncssSpec.js
@@ -39,4 +39,46 @@ describe("test minification", function() {
     expect(ncss(".float {margin: 0.6px 0.333pt 1.2em;}")).toEqual(".float{margin:.6px .333pt 1.2em}");
   });
 
+  it("should convert rgb() to hex", function() {
+    expect(ncss(".colors {color: rgb(123, 123, 123);background: none repeat scroll 0 0 rgb(255, 0, 0);}"))
+      .toEqual(".colors{color:#7b7b7b;background:none repeat scroll 0 0 #f00}");
+  });
+
+  it("should shorten hex colors", function() {
+    expect(ncss(".colors {border-color: #ffeedd}"))
+      .toEqual(".colors{border-color:#fed}");
+  });
+
+  it("should not convert rgba", function() {
+    expect(ncss(".cantouch {color: rgba(1, 2, 3, 4);}"))
+      .toEqual(".cantouch{color:rgba(1,2,3,4)}");
+  });
+
+  it("should not convert hex in filters", function() {
+    expect(ncss('.cantouch {filter: chroma(color="#FFFFFF");}'))
+      .toEqual('.cantouch{filter:chroma(color="#FFFFFF")}');
+  });
+
+  it("should only allow one charset", function() {
+    expect(ncss('@charset "utf-8";@charset "utf-8";'))
+      .toEqual('@charset "utf-8";');
+  });
+
+  it("should shorten none", function() {
+    expect(ncss(".short {border: none;background: none;outline: none;}"))
+      .toEqual(".short{border:0;background:0;outline:0}");
+  });
+
+  it("should allow hacks", function() {
+    expect(ncss("#hack {hack: 0;*hack: 0;_hack: 0;}"))
+      .toEqual("#hack{hack:0;*hack:0;_hack:0}");
+    expect(ncss('#elem {width: 100px; /* IE */voice-family: "\\"}\\"";voice-family:inherit;width: 200px; /* others */}\nhtml>body #elem {width: 200px; /* others */}'))
+      .toEqual('#elem{width:100px;voice-family:"\\"}\\"";voice-family:inherit;width:200px}html>body #elem{width:200px}');
+  });
+
+  it("should not remove the IE5/Mac hack", function() {
+    expect(ncss("html >/**/ body p {color: blue;}"))
+      .toEqual("html>/**/body p{color:blue}");
+  });
+
 });
